ae01ee9b5ed1121a0621dcb1bb054946d2027a8e
joacoague15 <joacoague15@gmail.com>
2023-09-04 19:01:17 +0100

    Start applying the combination system

--- src/App.js
+++ src/App.js
@@ -1,66 +1,68 @@
 import './App.css';
-import capybara_dummy from './assets/capybara_dummy.jpg';
-import hat1 from './assets/hat1.jpg';
-import hat2 from './assets/hat2.jpg';
-import hat3 from './assets/hat3.jpg';
-import hat4 from './assets/hat4.jpg';
+import penguin_dummy from './assets/penguin_dummy.png';
 
-import {useState} from "react";
+import head1_eyes2 from './assets/head1_eyes2.png';
+import head2_eyes1 from './assets/head2_eyes1.png';
+import head2_eyes2 from './assets/head2_eyes2.png';
+
+import { useState } from "react";
+
 
 function App() {
-    const [currentCapybaraHat, setCurrentCapybaraHat] = useState(capybara_dummy);
-    const allHats = [hat1, hat2, hat3, hat4];
+    const [currentPenguinHead, setCurrentPenguinHead] = useState(1);
+    const [currentPenguinEyes, setCurrentPenguinEyes] = useState(1);
 
-    const nextCapybaraHat = () => {
-        // If there is a hat, go to the next one
-        setCurrentCapybaraHat(allHats[allHats.indexOf(currentCapybaraHat) + 1]);
+    const PENGUIN_OPTIONS = [
+        { id: '1', src: penguin_dummy, head: 1, eyes: 1 },
+        { id: '2', src: head1_eyes2, head: 1, eyes: 2 },
+        { id: '3', src: head2_eyes1, head: 2, eyes: 1 },
+        { id: '3', src: head2_eyes2, head: 2, eyes: 2 },
+    ];
 
-        // If there is no hat, go to the first one
-        if (currentCapybaraHat === capybara_dummy)
-            setCurrentCapybaraHat(allHats[0]);
-
-        // If It's the last hat, go to the dummy one
-        if (currentCapybaraHat === allHats[allHats.length - 1])
-            setCurrentCapybaraHat(capybara_dummy);
+    const nextPenguinHead = () => {
+        setCurrentPenguinHead(currentPenguinHead + 1);
     }
 
-    const previousCapybaraHat = () => {
-        // If there is a hat, go to the previous one
-        setCurrentCapybaraHat(allHats[allHats.indexOf(currentCapybaraHat) - 1]);
+    const previousPenguinHead = () => {
+        setCurrentPenguinHead(currentPenguinHead - 1);
+    }
 
-        // If there is no hat, go to the last one
-        if (currentCapybaraHat === capybara_dummy)
-            setCurrentCapybaraHat(allHats[allHats.length - 1]);
+    const nextPenguinEyes = () => {
+        setCurrentPenguinEyes(currentPenguinEyes + 1);
+    }
 
-        // If It's the first hat, go to the dummy one
-        if (currentCapybaraHat === allHats[0])
-            setCurrentCapybaraHat(capybara_dummy);
+    const previousPenguinEyes = () => {
+        setCurrentPenguinEyes(currentPenguinEyes - 1);
+    }
+
+    const getPenguinImage = (head, eyes) => {
+        return PENGUIN_OPTIONS.find(option => option.head === head && option.eyes === eyes).src;
     }
 
   return (
     <div className="App">
         <div id="general-container">
             <div id="current-capybara-container">
-                <img id="current-capybara" src={currentCapybaraHat.toString()}  alt={capybara_dummy}/>
+                <img src={getPenguinImage(currentPenguinHead, currentPenguinEyes)}  alt={penguin_dummy}/>
             </div>
             <div>
                 <div id="capybara-changes-interface">
-                    <button className="selection-arrow" onClick={previousCapybaraHat}>&lt;</button>
+                    <button className="selection-arrow" onClick={previousPenguinHead}>&lt;</button>
                     <div className="parameter-to-change">Head</div>
-                    <button className="selection-arrow" onClick={nextCapybaraHat}>&gt;</button>
+                    <button className="selection-arrow" onClick={nextPenguinHead}>&gt;</button>
                 </div>
-                {/*<div id="capybara-changes-interface">*/}
-                {/*    <button className="selection-arrow">&lt;</button>*/}
-                {/*    <div className="parameter-to-change">Torso</div>*/}
-                {/*    <button className="selection-arrow">&gt;</button>*/}
-                {/*</div>*/}
+                <div id="capybara-changes-interface">
+                    <button className="selection-arrow" onClick={previousPenguinEyes}>&lt;</button>
+                    <div className="parameter-to-change">Eyes</div>
+                    <button className="selection-arrow" onClick={nextPenguinEyes}>&gt;</button>
+                </div>
                 {/*<div id="capybara-changes-interface">*/}
                 {/*    <button className="selection-arrow">&lt;</button>*/}
                 {/*    <div className="parameter-to-change">Foots</div>*/}
                 {/*    <button className="selection-arrow">&gt;</button>*/}
                 {/*</div>*/}
             </div>
-            <button id="submit-capybara"><b>Vestir carpincho</b></button>
+            <button id="submit-capybara"><b>Dress penguain</b></button>
         </div>
     </div>
   );
